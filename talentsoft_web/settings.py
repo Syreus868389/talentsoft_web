a144599c5f44169f0c935cc89526448cce584b28
Théo Pavlovic <pavlovic.theo@gmail.com>
2022-04-07 03:07:44 +0200

    fixed settings

--- talentsoft_web/settings.py
+++ talentsoft_web/settings.py
@@ -27,9 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # False if not in os.environ
 DEBUG = env('DEBUG')
 
